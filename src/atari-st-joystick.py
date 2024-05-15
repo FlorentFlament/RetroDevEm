a127319a915c7a744421a12ba9cb0b73f710403b
Florent Flament <contact@florentflament.com>
2024-05-15 22:51:14 +0200

    Add command line options to atari-st-joystick

--- src/atari-st-joystick.py
+++ src/atari-st-joystick.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import time
+import click
 from gpiozero import LED
 import inputdevice as idev
 
 BOARDS_CONFIG = {
     "v2.1": {
-        "port0": {
+        0: { # Port ID
             "down": 27,
             "up": 22,
             "left": 17,
             "right": 4,
             "fire": 10,
         },
-        "port1": {
+        1: {
             "down": 6,
             "up": 13,
             "left": 5,
@@ -26,8 +27,12 @@ def rpi_init(board_version, port_id):
     pins = BOARDS_CONFIG[board_version][port_id]
     return {k:LED(v) for k,v in pins.items()}
 
-def main(device, board_version, port_id):
-    port = rpi_init(board_version, port_id)
+@click.command()
+@click.option("--board", default="v2.1", help="Board revision.")
+@click.option("--device", default="/dev/input/event0", help="Input device to use.")
+@click.option("--port",  default=1, help="Board/Atari ST port to connect the joystick to.")
+def main(device, board, port):
+    signals = rpi_init(board, port)
     while True:
         try:
             dev = idev.InputDevice(device)
@@ -37,26 +42,26 @@ def main(device, board_version, port_id):
                 if  ev_type == idev.EV_ABS:
                     if   ev_code == idev.ABS_HAT0X:
                         print(f"HAT0X move: {ev_value}")
-                        if   ev_value == -1: port["left"].on()
-                        elif ev_value ==  1: port["right"].on()
+                        if   ev_value == -1: signals["left"].on()
+                        elif ev_value ==  1: signals["right"].on()
                         else: # ev_value == 0 when releasing a button
-                            port["left"].off()
-                            port["right"].off()
+                            signals["left"].off()
+                            signals["right"].off()
                     elif ev_code == idev.ABS_HAT0Y:
                         print(f"HAT0Y move: {ev_value}")
-                        if   ev_value == -1: port["up"].on()
-                        elif ev_value ==  1: port["down"].on()
+                        if   ev_value == -1: signals["up"].on()
+                        elif ev_value ==  1: signals["down"].on()
                         else: # ev_value == 0 when releasing a button
-                            port["down"].off()
-                            port["up"].off()
+                            signals["down"].off()
+                            signals["up"].off()
                 elif ev_type == idev.EV_KEY and ev_code == idev.BTN_SOUTH:
                         print(f"FIRE button: {ev_value}")
-                        if ev_value == 1: port["fire"].on()
-                        else: port["fire"].off()
+                        if ev_value == 1: signals["fire"].on()
+                        else: signals["fire"].off()
         except OSError as e:
             # Sometimes gamepads gets disconnected with crap cables
             print(f"Error getting event: {e}")
             time.sleep(0.2)
 
 if __name__ == "__main__":
-    main("/dev/input/event6", "v2.1", "port1")
+    main()
